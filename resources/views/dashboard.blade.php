8695ff4f5d5d757cc322465fc59b56fec02fafc1
iamrobbinleee <leerobinroxas8@gmail.com>
2025-09-26 15:10:10 +0800

    PP -Lee Modified UI of dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,20 +1,23 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight dark:bg-gray-900">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100" style="background-color: black;">
+            <div class="dark:bg-gray-900 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
                     <div class="card">
-                        <div class="card-header">
-                            <h3 style="color: black;">AVAILED SERVICES</h3>
+                        <div class="card-header bg-primary text-white">
+                            <h3>AVAILED SERVICES</h3>
                         </div>
-                        <div class="card-body" id="services-container">
+                        <div class="card-body">
                             <!-- Services will be loaded here -->
+                            <div class="row" id="services-container">
+
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -38,10 +41,25 @@ $(document).ready(function() {
             container.empty();
 
             availed_services.forEach(as => {
+                // let item = `
+                //     <div class="mb-2">
+                //         <span style="color:white;">${as.service_name}</span> - 
+                //         <a href="${as.service_url}" target="_blank" style="color:black;">${as.service_url}</a>
+                //     </div>
+                // `;
+
                 let item = `
-                    <div class="mb-2">
-                        <span style="color:white;">${as.service_name}</span> - 
-                        <a href="${as.service_url}" target="_blank" style="color:black;">${as.service_url}</a>
+                    <div class="col-md-4">
+                        <a href="${as.service_url}" target="_blank" style="color:black;">
+                            <div class="card">
+                                <div class="card-header bg-primary text-white">
+                                    <span>${as.service_name}</span>
+                                </div>
+                                <div class="card-body">
+                                    <span>${as.service_url}</span>
+                                </div>
+                            </div>
+                        </a>
                     </div>
                 `;
                 container.append(item);
